4fd9b198159830c2ab6ab903ce33271102c0b566
camilo chaparro <krpovmu@MBP-de-camilo.lan>
2018-08-20 23:04:14 -0500

    Start with the chapter 02

---
+++ ch02/01-assign_logfile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# The following file is assignmnet command
+logfile="monday.dat"
+
+echo "the value sof the logfile is:"
+
+# The dollar sign is used to access a variable's value
+echo $logfile
